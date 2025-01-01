0673572c32a192a0b92882c79d63494a00db0022
ravi kumar <boyaravikumar42@gmail.com>
2025-01-01 11:40:17 +0530

    toggle added

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@
 		* 
 		===================**************/
 		var typed= new Typed(".span",{
-			strings:["CS Enthusiast","Full - stack Developer"],
+			strings:["Full - stack Developer","Web Developer"],
 			typeSpeed:80,
 			backSpeed:80,
 			backDelay:800,
@@ -12,23 +12,11 @@
 		})
         
         /*===== menu icon scripting======*/
-const menuEnable=(e)=>
-{
-    let bar=document.getElementById('bar') 
-    let cross=document.getElementById('bar1') 
-    let cont=document.getElementById('navbar');
-    bar.style.display='none';
-    cross.style.display='block';
-    cont.style.display='flex';
-    
-}
-const menuDisable=()=>
-    {
         let bar=document.getElementById('bar');
-        let cross=document.getElementById('bar1');
-        cross.style.display='none'; 
-        bar.style.display='block'; 
-        let cont=document.getElementById('navbar');
-        cont.style.display='none';
-        
-    }
+        let nav=document.getElementById('navbar');
+
+        bar.addEventListener('click',()=>{
+            bar.classList.toggle('fa-bars-staggered');
+            bar.classList.toggle('fa-xmark');
+            nav.classList.toggle('active');
+        });
